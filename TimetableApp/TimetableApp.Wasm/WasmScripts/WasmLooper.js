0ccb06eac260c97fb9f03e8e93767a046b4f213f
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2021-04-19 09:41:26 +0700

    chore: Removed unnecessary WasmLooper hack

--- TimetableApp/TimetableApp.Wasm/WasmScripts/WasmLooper.js
+++
@@ -1,5 +0,0 @@
-﻿const loopMethod = Module.mono_bind_static_method('[TimetableApp.Wasm] TimetableApp.Wasm.WasmLooper:OnLoop');
-
-function StartLoop(milliseconds) {
-    setInterval(loopMethod, milliseconds);
-}
