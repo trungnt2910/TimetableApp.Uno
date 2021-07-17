64243ba7a79efcdde9def7b2f152309fd3835e49
trungnt2910 <trungnt2910@users.noreply.github.com>
2021-07-17 12:24:14 +0000

    deploy: 0c12227ac3458d4be7be399bea85ee77c4683332

---
+++ package_71e23c556e8799303487c6315d95e4bed1713147/mono-config.js
@@ -0,0 +1,6 @@
+config = {
+ 	vfs_prefix: "managed",
+ 	deploy_prefix: "managed",
+ 	enable_debugging: 0,
+ 	file_list: [ "Uno.Wasm.TimezoneData.dll","netstandard.dll","mscorlib.dll","System.dll","Mono.Security.dll","System.Xml.dll","System.Numerics.dll","System.Core.dll","WebAssembly.Net.WebSockets.dll","WebAssembly.Bindings.dll","System.Data.dll","System.Drawing.Common.dll","System.Net.Http.dll","System.Net.Http.WebAssemblyHttpHandler.dll","System.Runtime.Serialization.dll","System.Xml.Linq.dll","TimetableApp.Wasm.dll","Uno.UI.Runtime.WebAssembly.dll","Uno.UI.dll","Uno.Xaml.dll","Uno.Foundation.dll","Uno.Core.dll","System.Collections.Immutable.dll","CommonServiceLocator.dll","Microsoft.Extensions.Logging.Abstractions.dll","Microsoft.Extensions.Logging.dll","Microsoft.Extensions.DependencyInjection.Abstractions.dll","Microsoft.Extensions.Options.dll","Microsoft.Extensions.Primitives.dll","System.Runtime.CompilerServices.Unsafe.dll","Microsoft.Extensions.DependencyInjection.dll","Microsoft.Bcl.AsyncInterfaces.dll","Uno.dll","Uno.Foundation.Runtime.WebAssembly.dll","Uno.Diagnostics.Eventing.dll","Microsoft.Toolkit.Uwp.UI.Controls.DataGrid.dll","System.ComponentModel.DataAnnotations.dll","Uno.UI.Toolkit.dll","Newtonsoft.Json.dll","Uno.UI.FluentTheme.dll","Uno.Extensions.Logging.WebAssembly.Console.dll" ],
+}
