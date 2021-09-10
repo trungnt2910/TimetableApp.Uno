d47ff91156f321979eeba2b5e999a5c1e6c70290
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2021-09-10 13:38:24 +0700

    fix: Fixed Powershell script

--- TimetableApp/TimetableApp.Wasm/CopyHtmlScript.ps1
+++ TimetableApp/TimetableApp.Wasm/CopyHtmlScript.ps1
@@ -1,2 +1,6 @@
-$s = Get-ChildItem -Name -Directory bin\Release\net5.0\publish\package*;
-Copy-Item .\package_index.html (".\bin\Release\net5.0\publish\" + $s + "\index.html")
+# Fix for Github Pages. Local builds do not need this fix.
+
+$s = Get-ChildItem -Name -Directory ($PSScriptRoot + "\bin\Release\net5.0\publish\package*"); 
+$htmlFile = $PSScriptRoot + "\bin\Release\net5.0\publish\" + $s + "\index.html";
+echo ("Copying file to " + $PSScriptRoot + $htmlFile);
+Copy-Item ($PSScriptRoot + "\package_index.html") $htmlFile
